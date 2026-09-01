1e65997d1729aa7095143c444e3acadf1b5bb70d
rami-herrera <elreimonh@gmail.com>
2026-09-01 13:26:45 -0300

    Practica 1/9

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let pepe = {};
+
+let monica = [1, 2, 3, 4, 5];
+
+console.log(pepe);
+console.log(monica);
+
+console.log(monica[3]);
+
+monica[3] = "Hola";
+
+console.log(monica);
+
+console.log(monica[3]);
+// let numero_usuario = prompt("Ingresar un número entero");
+// console.log("El número ingresado es: " + numero_usuario);
+
+// let numero_usuario2 = parseInt(numero_usuario);
+// console.log("El número ingresado es: ");
+// console.log(numero_usuario2);
+
+// let variable1 = "22";
+// let variable2 = 22;
+
+// console.log(variable1);
+// console.log(variable2);
+
+// if (variable1 == variable2) {
+//     console.log("Son iguales");
+// }
+// else {
+//     console.log("No son iguales");
+// }
+
+// variable1 = 22;
+
+// if (variable1 === variable2) {
+//     console.log("Son iguales");
+// }
+// else {
+//     console.log("No son iguales");
+// }
